248f9e94dfa87689e65940bf60741b5d488c31a5
Adrien Champion <adrien.champion@email.com>
2017-07-03 19:00:02 +0900

    Funny macro

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,87 @@ mod consts {
 }
 
 pub mod safe ;
+
+
+
+
+#[doc = r#"Performs something special on the first element of an iterator, and
+then works on the rest.
+
+```
+#[macro_use]
+extern crate mylib ;
+
+fn main() {
+  let input = "lines\nseveral\nover\nmsg" ;
+
+  let expected = "msg\nover\nseveral\nlines" ;
+  let mut buff ;
+  let result = for_first!(
+    input.lines() => {
+      |fst_line| buff = format!("{}", fst_line),
+      then |nxt_line| buff = format!("{}\n{}", nxt_line, buff),
+      yild buff
+    } else "".into()
+  ) ;
+
+  assert_eq!( result, expected ) ;
+
+  // Alternatively:
+  let new_expected = "msg:over:one:line" ;
+  let mut buff = String::new() ;
+  for_first!(
+    result.lines() => {
+      |fst_line| buff.push_str( & format!("{}", fst_line) ),
+      then |nxt_line| {
+        buff.push(':') ;
+        if nxt_line == "several" {
+          buff.push_str("one")
+        } else {
+          buff.push_str( & format!("{}", nxt_line) )
+        }
+      }
+    }
+  ) ;
+  let obsolete_s = buff.pop() ;
+  assert_eq!( obsolete_s, Some('s') ) ;
+
+  assert_eq!( buff, new_expected ) ;
+}
+```
+"#]
+#[macro_export]
+macro_rules! for_first {
+  (
+    $iter:expr => {
+      |$fst:pat| $e_fst:expr,
+      then |$thn:pat| $e_thn:expr,
+      yild $e_yld:expr $(,)*
+    } else $e_els:expr
+  ) => ({
+    let mut iter = $iter ;
+    if let Some($fst) = iter.next() {
+      $e_fst ;
+      for $thn in iter {
+        $e_thn
+      }
+      $e_yld
+    } else {
+      $e_els
+    }
+  }) ;
+  (
+    $iter:expr => {
+      |$fst:pat| $e_fst:expr,
+      then |$thn:pat| $e_thn:expr $(,)*
+    }
+  ) => (
+    for_first!{
+      $iter => {
+        |$fst| $e_fst,
+        then |$thn| $e_thn,
+        yild (())
+      } else (())
+    }
+  ) ;
+}
